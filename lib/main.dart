02ec7746a03832ce52a929579f16fce213135cd1
ggguille <999uille@gmail.com>
2020-07-20 16:22:56 +0200

    Tapping into Widget Properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.amber,
+                  backgroundImage: AssetImage('images/avatar_icon.png')
+              ),
+              Text(
+                'Guillermo Granado',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
