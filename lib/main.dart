cfd40239416d012ad6e9f002160ee4bb38e8af2c
ggguille <999uille@gmail.com>
2020-07-20 17:24:23 +0200

    Adding Material Icons with the Icon Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                   radius: 50.0,
@@ -39,6 +40,50 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'Source Sans Pro'
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '600 12 43 99',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'guille@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
