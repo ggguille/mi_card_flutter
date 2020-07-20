31ef486a5d8da308c69839e77c3c4877db63b83e
ggguille <999uille@gmail.com>
2020-07-20 17:39:38 +0200

    Flutter Card & ListTile Widget

--- lib/main.dart
+++ lib/main.dart
@@ -40,48 +40,41 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'Source Sans Pro'
                 ),
               ),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '600 12 43 99',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                      ),
-                    )
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100
                 ),
               ),
-              Container(
-                color: Colors.white,
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '600 12 43 99',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
                     ),
-                    SizedBox(
-                      width: 10.0,
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'guille@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
                     ),
-                    Text(
-                      'guille@gmail.com',
-                      style: TextStyle(
-                          color: Colors.teal.shade900,
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               )
             ],
